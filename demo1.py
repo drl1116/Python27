6df14c51fc86229b9ebded87e177809401c609df
Alice-PC\Alice <drl1116@163.com>
2017-09-01 17:04:42 +0800

    一个小测试题目

--- demo1.py
+++ demo1.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
 print("Hello World！")
+for i in range(1, 101):
+    if i % 15 == 0:
+        print("3 and 5")
+    elif i % 3 == 0:
+        print("3333")
+    elif i % 5 == 0:
+        print("5555")
+    else:
+        print(i)
--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-# -*- coding: utf-8 -*-
-# 这是一个测试页面！！！
+# -- coding: utf-8 --
+# 解决列表中输出中文乱码问题。只适用于Python2
+import json
+lists = ["one", "two", "three", "four", "five"]
+print lists[0]
+
+lists[0] = "six"
+print lists
+lists.append("别闹，我才是萌新！")
+print json.dumps(lists, encoding="utf-8", ensure_ascii=False)
