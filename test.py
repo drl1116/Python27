8644746860a52f228d24718a5405534dfd217b89
Alice <drl1116@163.com>
2017-09-07 17:00:16 +0800

    修正代码，使其符合PEP8规范；

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-# -- coding: utf-8 --
+# -*- coding: utf-8 -*-
 # 解决列表中输出中文乱码问题。只适用于Python2
 import json
 lists = ["one", "two", "three", "four", "five"]
-print lists[0]
+print(lists[0])
 
 lists[0] = "six"
-print lists
+print(lists)
 lists.append("别闹，我才是萌新！")
-print json.dumps(lists, encoding="utf-8", ensure_ascii=False)
+print(json.dumps(lists, encoding="utf-8", ensure_ascii=False))
